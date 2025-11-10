2f4434c44e5368c3da23018fed308b6f66d5ae7a
PritGD2611 <pritgd26@gmail.com>
2025-11-10 22:24:58 +0530

    V 0.5
    Animations are done.

--- Bosozoku/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Bosozoku/Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -5,21 +5,21 @@ using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : MonoBehaviour
-	{
+    public class StarterAssetsInputs : MonoBehaviour
+    {
 
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+        [Header("Character Input Values")]
+        public Vector2 move;
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+        public bool cursorInputForLook = true;
 
         public bool lightAttack;
         public bool heavyAttack;
@@ -27,37 +27,42 @@ namespace StarterAssets
         public bool blockReleased;
 
 
+
+
+
 #if ENABLE_INPUT_SYSTEM
         public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
+        {
+            MoveInput(value.Get<Vector2>());
+        }
 
-		public void OnLook(InputValue value)
-		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
-		}
+        public void OnLook(InputValue value)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(value.Get<Vector2>());
+            }
+        }
 
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
+        public void OnJump(InputValue value)
+        {
+            JumpInput(value.isPressed);
+        }
 
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
-        public void OnLightAttack(InputValue value) 
-		{
-			lightAttack = value.isPressed;
-		}
-        public void OnHeavyAttack(InputValue value) 
-		{ 
-			heavyAttack = value.isPressed;
-		}
+        public void OnSprint(InputValue value)
+        {
+            SprintInput(value.isPressed);
+        }
+        public void OnLightAttack(UnityEngine.InputSystem.InputValue value)
+        {
+            lightAttack = value.isPressed;
+        }
+
+        public void OnHeavyAttack(UnityEngine.InputSystem.InputValue value)
+        {
+            heavyAttack = value.isPressed;
+        }
+
         public void OnBlock(InputValue value)
         {
             if (value.isPressed) { blockPressed = true; }
@@ -68,34 +73,36 @@ namespace StarterAssets
 
 
         public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
+        {
+            move = newMoveDirection;
+        }
 
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
 
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
 
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
 
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
-	
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+
+
+    }
+
 }
